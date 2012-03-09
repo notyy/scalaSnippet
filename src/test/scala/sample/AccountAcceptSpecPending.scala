07184d18a0e45e10db8fe529ae04019f61b952bf
notyy <notyycn@gmail.com>
2012-03-09 14:58:30 +0800

    fix a naming error

--- src/test/scala/sample/AccountAcceptSpecPending.scala
+++ src/test/scala/sample/AccountAcceptSpecPending.scala
@@ -3,7 +3,7 @@ import org.scalatest.FeatureSpec
 import org.scalatest.GivenWhenThen
 import org.scalatest.matchers.ShouldMatchers
 
-class AccountAcceptSpec extends FeatureSpec with GivenWhenThen with ShouldMatchers {
+class AccountAcceptSpecPending extends FeatureSpec with GivenWhenThen with ShouldMatchers {
   feature("账户可以被创建，检查和相互转账") {
     info("作为账户体系管理者")
     info("我要求账户体系能够确保账户内的资金可以可靠的流转，账户状态可以被检查")
