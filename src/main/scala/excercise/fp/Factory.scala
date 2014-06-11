d9e603ddcf7a6c4901c3c37cb292ee870e1683eb
notyy <notyycn@gmail.com>
2014-06-11 22:22:19 +0800

    roughly implement

---
+++ src/main/scala/excercise/fp/Factory.scala
@@ -0,0 +1,107 @@
+package excercise.fp
+
+import scala.io.Source
+
+object Factory {
+  type FileName = String
+
+  def processInput: FileName => Unit = fileName => {
+    createPlan(Source.fromFile(fileName).getLines().toList).foreach(formatShowPlan)
+  }
+
+  def createPlan: List[String] => List[Plan] = requestDesc => {
+    val (resources, orders) = parseRequest(requestDesc)
+    createPlan(naiveCreatePlan(resources), orders)
+  }
+
+  def createPlan(f: Order => Plan, orders: List[Order]): List[Plan] = {
+    orders.map(f)
+  }
+
+  trait MachineType
+  case object GMachine extends MachineType
+  case object MMachine extends MachineType
+  case object RMachine extends MachineType
+  case object PMachine extends MachineType
+  case object Cooler extends MachineType
+
+  type Quantity = Int
+  type Product = String
+
+  case class Order(id: Int, products: List[Product])
+
+  case class Machine(mType: MachineType,id: Int)
+
+  case class Plan(orderId: Int, workProcess: List[WorkProcess])
+  case class WorkProcess(machine: Machine, products: List[Product])
+
+  type ResourceDesc = (MachineType, Quantity)
+
+  def parseRequest: List[String] => (List[ResourceDesc], List[Order]) = src => {
+    val index = src.indexWhere(_.startsWith("Order:"))
+    val (resourceDescStr, orderStrs) = src.splitAt(index)
+    (resourceDescStr.map(parseResourceDesc), orderStrs.zipWithIndex.map{case (order, id) => parseOrderDesc(id)(order)})
+  }
+
+  def parseResourceDesc: String => ResourceDesc = s => {
+    val splitted = s.split(":")
+    (strToMachineType(splitted.head), splitted.last.trim.toInt)
+  }
+
+  val machineTypeStrMap: Map[String, MachineType] = Map("G" -> GMachine, "M" -> MMachine, "R" -> RMachine, "P" -> PMachine)
+
+  def strToMachineType: String => MachineType = machineTypeStrMap(_)
+
+  def parseOrderDesc: Int => String => Order = orderId => str => {
+    Order(orderId, str.split(":").last.trim.split(" ").toList)
+  }
+
+  def naiveCreatePlan: List[ResourceDesc] => Order => Plan = rDesc => order => {
+    val workProcesses = rDesc.flatMap(initializeMachine)
+    naiveCreatePlanFromWorkProcess(workProcesses)(order)
+  }
+
+  def naiveCreatePlanFromWorkProcess: List[WorkProcess] => Order => Plan = workProcesses => order => {
+    Plan(order.id, arrange(workProcesses)(order.products))
+  }
+
+  def arrange: List[WorkProcess] => List[Product] => List[WorkProcess] = workProcesses => products => {
+    products.foldLeft(workProcesses)((wps, product) => arrangeProduction(wps)(product))
+  }
+
+  def arrangeProduction: List[WorkProcess] => Product => List[WorkProcess] = workProcesses => product => {
+    workProcesses.groupBy(_.machine.mType).values.map(
+      workProcessByType => assignProduct(workProcessByType, product)).flatten.toList.sortBy(_.machine.toString)
+  }
+
+  def assignProduct(processes: List[WorkProcess], product: Product): List[WorkProcess] = {
+    val sorted = processes.sortBy(_.products.size)
+    val idlest: WorkProcess = sorted(0)
+    sorted.updated(0, WorkProcess(idlest.machine, idlest.products :+ product))
+  }
+
+  def initializeMachine: ResourceDesc => List[WorkProcess] = {
+    case (machineType, quantity) => (1 to quantity).map(i => WorkProcess(Machine(machineType, i), Nil)).toList
+  }
+
+  def formatShowPlan: Plan => Unit = plan => {
+    println(s"Order #${plan.orderId}")
+    plan.workProcess.map(w => s"${machine2Str(w.machine)}: ${w.products.mkString("\t")}").foreach(println)
+    println(s"Total: ${calcTime(plan)} hours")
+  }
+
+  def machine2Str: Machine => String = m => s"${m.mType.toString.head}${m.id}"
+
+  type Hour = Int
+  val productMachineTime: Map[Product,Map[MachineType,Hour]] = Map(
+    "zteT" -> Map(GMachine -> 3,MMachine -> 3,RMachine -> 1,Cooler -> 2,PMachine -> 1),
+    "zteW" -> Map(GMachine -> 2,MMachine -> 2,RMachine -> 3,Cooler -> 2,PMachine -> 2),
+    "zteX" -> Map(GMachine -> 3,MMachine -> 3,RMachine -> 1,Cooler -> 5,PMachine -> 1),
+    "zteY" -> Map(GMachine -> 1,MMachine -> 1,RMachine -> 4,Cooler -> 3,PMachine -> 3),
+    "zteZ" -> Map(GMachine -> 2,MMachine -> 3,RMachine -> 2,Cooler -> 1,PMachine -> 1)
+  )
+
+  def calcTime:Plan => Int = plan => {
+    (for(w <- plan.workProcess; p <- w.products) yield productMachineTime(p)(w.machine.mType)).sum
+  }
+}
---
+++ src/test/scala/excercise/fp/packageFpTest.scala
@@ -0,0 +1,85 @@
+package excercise.fp
+
+import org.scalatest.{ShouldMatchers, FunSpec}
+import Factory._
+
+class packageFpTest extends FunSpec with ShouldMatchers {
+  describe("object Factory defines many functions") {
+    describe("createPlan") {
+      it("should create order processing plan" +
+        " for given ProductionResource and OrderRequest") {
+        val request = """G:	2
+                        |M:	1
+                        |R:	1
+                        |P:	1
+                        |Order: zteT zteW zteX""".stripMargin('|').lines.toList
+        val expectedResult = """Order＃1
+                               |G1:	 	zteT	zteW
+                               |G2:		zteX
+                               |M：	zteX 	zteW	zteT
+                               |R：		zteX 	zteW	zteT
+                               |P：		zteX 	zteW	zteT
+                               |Total: 17 hours""".stripMargin('|').lines.toList
+        pending
+        createPlan(request) shouldBe expectedResult
+      }
+
+      describe("parseResourceDesc") {
+        it("can parse G: 2 to (G,2)") {
+          parseResourceDesc("G:  2") shouldBe(GMachine, 2)
+        }
+      }
+
+      describe("initializeMachine") {
+        it("can convert machineType&quantity to machine instance") {
+          val rd: ResourceDesc = (GMachine, 2)
+          val machines = initializeMachine(rd)
+          machines.size shouldBe 2
+          machines(0).machine.id shouldBe 1
+          machines(0).products shouldBe empty
+          machines(1).machine.id shouldBe 2
+          machines(1).products shouldBe empty
+        }
+      }
+      describe("arrangeProduction") {
+        it("can assign product to machine") {
+          val workProcesses: List[WorkProcess] = List(
+            WorkProcess(Machine(GMachine, 1), Nil),
+            WorkProcess(Machine(GMachine, 2), Nil),
+            WorkProcess(Machine(MMachine, 1), Nil),
+            WorkProcess(Machine(RMachine, 1), Nil),
+            WorkProcess(Machine(PMachine, 1), Nil)
+          )
+
+          val zteTArranged = arrangeProduction(workProcesses)("zteT")
+          zteTArranged(0).products shouldBe List("zteT")
+          zteTArranged(1).products shouldBe Nil
+          zteTArranged(2).products shouldBe List("zteT")
+          zteTArranged(3).products shouldBe List("zteT")
+          zteTArranged(4).products shouldBe List("zteT")
+
+          val zteWArranged = arrangeProduction(zteTArranged)("zteW")
+          zteWArranged.foreach(println)
+          zteWArranged(0).products shouldBe List("zteT")
+          zteWArranged(1).products shouldBe List("zteW")
+          zteWArranged(2).products shouldBe List("zteT", "zteW")
+          zteWArranged(3).products shouldBe List("zteT", "zteW")
+          zteWArranged(4).products shouldBe List("zteT", "zteW")
+        }
+      }
+
+      describe("formatShowPlan") {
+        it("should pretty print a plan") {
+          val workProcess = List(
+            WorkProcess(Machine(GMachine, 1), List("zteT")),
+            WorkProcess(Machine(GMachine, 2), List("zteW")),
+            WorkProcess(Machine(MMachine, 1), List("zteT", "zteW")),
+            WorkProcess(Machine(PMachine, 1), List("zteT", "zteW")),
+            WorkProcess(Machine(RMachine, 1), List("zteT", "zteW")))
+          val plan = Plan(1, workProcess)
+          formatShowPlan(plan)
+        }
+      }
+    }
+  }
+}
