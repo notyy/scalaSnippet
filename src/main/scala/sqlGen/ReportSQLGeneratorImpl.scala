af992b624572d7eb134a822105ef57dcb618224e
notyy <notyycn@gmail.com>
2014-07-01 14:58:23 +0800

    refactor report sql generator to make it even modular

--- src/main/scala/sqlGen/ReportSQLGeneratorImpl.scala
+++ src/main/scala/sqlGen/ReportSQLGeneratorImpl.scala
@@ -1,10 +1,14 @@
 package sqlGen
-import sqlGen.Domain.{Granularity, SQL, TimeSegment}
+import sqlGen.Domain.{SQL, TimeSegment}
 
 trait ReportSQLGenerator {
+  def genSQL(timeSegments: List[TimeSegment]): SQL
+}
+
+trait ReportSQLGeneratorImpl extends ReportSQLGenerator {
   this: GranularitySQLGenerator with TimeSegmentSQLGenerator with DBConfigLoader =>
 
-  def genSQL(granularity: Granularity, timeSegments: List[TimeSegment]): SQL = {
+  override def genSQL(timeSegments: List[TimeSegment]): SQL = {
     SQL(s"SELECT * FROM $calls WHERE granularity = ${genGranularitySQL.str}" +
       s" AND ${genTimeSegmentsSQL(timeSegments).str} AND configInDB = ${loadConfig.str}")
   }
--- src/test/scala/sqlGen/ReportSQLGeneratorImplTest.scala
+++ src/test/scala/sqlGen/ReportSQLGeneratorImplTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.mock.MockitoSugar
 import org.scalatest.{ShouldMatchers, FunSpec}
 import sqlGen.Domain._
 
-class ReportSQLGeneratorTest extends FunSpec with ShouldMatchers {
+class ReportSQLGeneratorImplTest extends FunSpec with ShouldMatchers {
   
   trait SampleDBConfigLoader extends DBConfigLoader{
     override def loadConfig: SQLPart = SQLPart("'dbproperties'")
@@ -15,11 +15,11 @@ class ReportSQLGeneratorTest extends FunSpec with ShouldMatchers {
       val granularity:Granularity = RNCGranularity
 
       val reportGenerator = granularity match {
-        case RNCGranularity => new ReportSQLGenerator with TimeSegmentSQLGenerator with RNCGranularitySQLGenerator with SampleDBConfigLoader {}
-        case CELLGranularity => new ReportSQLGenerator with TimeSegmentSQLGenerator with CELLGranularitySQLGenerator with SampleDBConfigLoader {}
+        case RNCGranularity => new ReportSQLGeneratorImpl with TimeSegmentSQLGenerator with RNCGranularitySQLGenerator with SampleDBConfigLoader {}
+        case CELLGranularity => new ReportSQLGeneratorImpl with TimeSegmentSQLGenerator with CELLGranularitySQLGenerator with SampleDBConfigLoader {}
       }
 
-      val sql = reportGenerator.genSQL(RNCGranularity, List(TimeSegment("2014-03-04", "2014-04-05")))
+      val sql = reportGenerator.genSQL(List(TimeSegment("2014-03-04", "2014-04-05")))
       sql shouldBe SQL("SELECT * FROM calls WHERE granularity = 'RNC' " +
         "AND (timeSegments < '2014-04-05' AND timeSegments > '2014-03-04') AND configInDB = 'dbproperties'")
     }
