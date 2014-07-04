55aafef30f238c3b82f3e08dbf9d0eba32207d7c
notyy <notyycn@gmail.com>
2014-07-04 11:37:47 +0800

    add a small sample of data analyze

--- src/main/scala/stats/pregnent/PregnancyAnalyze.scala
+++ src/main/scala/stats/pregnent/PregnancyAnalyze.scala
@@ -5,11 +5,25 @@ object PregnancyAnalyze {
 
   def extract(str: String): Pregnancy = {
     Pregnancy(
-      str.substring(0,12).trim.toInt,
-      str.substring(274,276).trim.toInt,
-      str.substring(276,277).trim.toInt,
-      str.substring(277,279).trim.toInt,
+      transform(str, 0,12, _.toInt).get,
+      transform(str,274,276,_.toInt).get,
+      transform(str,276,277,_.toInt).get,
+      transform(str,277,279,_.toInt).getOrElse(-1),
       str.substring(422,440).trim.toFloat
     )
   }
+
+  private def transform[A](src: String,startIdx: Int, endIdx: Int, f: String => A):Option[A] = {
+    val v = src.substring(startIdx, endIdx).trim
+    if(v.isEmpty) None else Some(f(v))
+  }
+
+  def liveBirthCount(pregnancies: List[Pregnancy]): Int = pregnancies.count(_.outcome == 1)
+
+  def liveBirthGroupByBirthOrder(pregnancies: List[Pregnancy]): (List[Pregnancy], List[Pregnancy]) = {
+    val lives = pregnancies.filter(_.outcome >= 1)
+    (lives.filter(_.birthOrd == 1), lives.filter(_.birthOrd != 1))
+  }
+
+  def avgPrgLength(pregnancies: List[Pregnancy]): Float = pregnancies.map(_.prgLength).sum / pregnancies.size
 }
--- src/test/scala/stats/pregnent/PregnancyAnalyzeSpec.scala
+++ src/test/scala/stats/pregnent/PregnancyAnalyzeSpec.scala
@@ -14,5 +14,36 @@ class PregnancyAnalyzeSpec extends FunSpec with ShouldMatchers {
       p.birthOrd shouldBe 1
       p.finalWgt shouldBe 6448.271F
     }
+    it("if birth order string is empty, result should be -1"){
+      val s = "           1 1     6 1     11093 1084     9 039 9   0  1 813             1093 13837                        1                5                                                                        116610931166 9201093                111             3    1   12  11         5391  10933316108432411211     2995 1212 69544441116122222 2 2224693215    000000000000000000000000000000000000003410.38939935294273869.3496019830486 6448.2711117047512 91231"
+      val p = extract(s)
+      p.birthOrd shouldBe -1
+    }
+    it("can calculate times of live birth"){
+      liveBirthCount(List(
+        Pregnancy(1,1,outcome = 1,1,1),
+        Pregnancy(1,1,outcome = 0,1,1)
+      )) shouldBe 1
+    }
+    it("can split live birth records to first birth group and others"){
+      val pregnancies = List(
+        Pregnancy(1,1,outcome = 1,birthOrd = 1,1),
+        Pregnancy(2,1,outcome = 1,birthOrd = 2,1),
+        Pregnancy(3,1,outcome = 1,birthOrd = 1,1),
+        Pregnancy(3,1,outcome = 1,birthOrd = 3,1),
+        Pregnancy(3,1,outcome = 1,birthOrd = 4,1),
+        Pregnancy(4,1,outcome = 0,1,1)
+      )
+      val (firstLives, others) = liveBirthGroupByBirthOrder(pregnancies)
+      firstLives should have size 2
+      others should have size 3
+    }
+    it("can calculate avg pregLength"){
+      val pregnancies = List(
+        Pregnancy(1,prgLength = 30,1,1,1),
+        Pregnancy(2,prgLength = 40,1,2,1)
+      )
+      avgPrgLength(pregnancies) shouldBe 35f
+    }
   }
 }
