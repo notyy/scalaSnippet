2c20563442d7209f02cdff84d80fc8791b9a8a26
notyy <notyycn@gmail.com>
2014-08-20 17:19:36 +0800

    remove useless code

--- src/main/scala/patterns/Persist.scala
+++ src/main/scala/patterns/Persist.scala
@@ -9,10 +9,4 @@ trait Persist[String] {
 object Persist {
 
   def apply(path: Path, content: String): Persist[String] = ???
-
-  implicit class PoliteInt(val value: Int) {
-    def sayHello() = println(s"hello, from $value")
-  }
-
-  1.sayHello()
 }
