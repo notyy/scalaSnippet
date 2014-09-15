56fe8fbeed66c001b573a9d220a90de8a474ebac
notyy <notyycn@gmail.com>
2014-09-15 20:24:49 +0800

    fix a small bug

--- src/main/scala/patterns/ReadSupport.scala
+++ src/main/scala/patterns/ReadSupport.scala
@@ -3,5 +3,5 @@ package patterns
 import scala.io.Source
 
 trait ReadSupport {
-  def read(path:Path): String = Source.fromFile(path.value).mkString.toUpperCase
+  def read(path:Path): String = Source.fromFile(path.value).mkString
 }
