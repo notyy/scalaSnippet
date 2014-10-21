00cf305dddeda1b81d6ed44e794a898f1729ff21
notyy <notyycn@gmail.com>
2014-10-21 16:38:57 +0800

    add a promise sample

--- src/test/scala/concurrency/future/大雄.scala
+++ src/test/scala/concurrency/future/大雄.scala
@@ -1,8 +1,11 @@
 package concurrency.future
 
+import java.util.Date
+
 import com.typesafe.scalalogging.slf4j.LazyLogging
 import scala.concurrent._
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
 
 case class Money(amount: Double)
 
@@ -10,30 +13,26 @@ case class Shoe(brand: String)
 
 case class BasketBall(id: Int)
 
-object XiaoMing extends App with LazyLogging {
-
+object 大雄 extends App with LazyLogging {
+  val start = new Date().getTime
   takeCloth()
+  val money = takeMoneyFromBank(500)
+  buyShoesFromHongkong(money)
   takeBasketBall()
-  takeShoes()
   playBasketBall()
+  val end = new Date().getTime
+  logger.info(s"total duration: ${end - start}")
 
   def takeCloth(): Unit = logger.info("taking up adidas")
 
-  def takeShoes(): Unit = {
-    logger.info("taking Shoes")
-    val money = takeMoneyFromBank(500)
-    val shoes = buyShoesFromHongkong(money)
-    logger.info(s"got shoes $shoes")
-  }
-
   def takeBasketBall(): Unit = {
     logger.info("taking basketBall")
-    val basketBall = borrowFromXiaoHong()
+    val basketBall = borrowFrom静香()
     logger.info(s"got basketBall $basketBall")
   }
 
-  def borrowFromXiaoHong(): BasketBall = {
-    logger.info("borrowing basketBall from XiaoHong")
+  def borrowFrom静香(): BasketBall = {
+    logger.info("borrowing basketBall from 静香")
     Thread.sleep(5000)
     val basketBall = BasketBall(1)
     logger.info(s"basketBall $basketBall successfully borrowed from XiaoHong")
@@ -50,7 +49,7 @@ object XiaoMing extends App with LazyLogging {
 
   def takeMoneyFromBank(amount: Double): Money = {
     logger.info("taking money from bank")
-    Thread.sleep(1000)
+    Thread.sleep(2000)
     val money = Money(amount)
     logger.info(s"money taken from bank $money")
     money
---
+++ src/test/scala/concurrency/promise/Double11.scala
@@ -0,0 +1,53 @@
+package concurrency.promise
+
+import com.typesafe.scalalogging.slf4j.LazyLogging
+
+import scala.concurrent._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+
+case class Money(amount: Double)
+
+case class Shoe(brand: String)
+
+object Double11 extends App with LazyLogging {
+  logger.info("演出开始")
+  val p = Promise[Money]()
+  val f = p.future
+
+  val 大雄的剧本 = Future {
+    logger.info("大雄去给小静打钱")
+    Thread.sleep(2000)
+    p.success(Money(500))
+    logger.info("大雄出去玩了")
+    Thread.sleep(2000)
+  }
+
+  val 小静的剧本 = Future {
+    logger.info("小静在逛淘宝")
+    Thread.sleep(3000)
+    f.onSuccess {
+      case money => buyShoesFromHongkong(money)
+    }
+    logger.info("小静给大雄做晚饭")
+    Thread.sleep(2000)
+  }
+
+  大雄的剧本.onSuccess{
+    case _ => logger.info("大雄回家了")
+  }
+
+  小静的剧本.onSuccess{
+    case _ => logger.info("小静和大雄高兴的一起吃饭")
+  }
+
+  Thread.sleep(6000)
+  logger.info("演出结束")
+
+  def buyShoesFromHongkong(money: Money): Shoe = {
+    logger.info("buying shoes from hongkong")
+    val shoes = Shoe("Asics")
+    logger.info(s"shoes bought from hongkong: $shoes")
+    shoes
+  }
+}
