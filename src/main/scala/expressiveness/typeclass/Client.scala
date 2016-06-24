d32b3483c1bcb6b9c4bd3ab2d3b0acad908f1d02
notyy <notyycn@gmail.com>
2016-06-24 14:52:24 +0800

    refactor typeclass example

--- src/main/scala/expressiveness/typeclass/Client.scala
+++ src/main/scala/expressiveness/typeclass/Client.scala
@@ -3,6 +3,6 @@ package expressiveness.typeclass
 import ShapeModule._
 
 object Client {
-  def printArea[T <: Shape](shape: T)(implicit m: HaveArea[T]) = println(m.area(shape))
-  def printPerimeter[T <: Shape](shape: T)(implicit m: HavePerimeter[T]) = println(m.perimeter(shape))
+  def printArea[T](shape: T)(implicit m: HaveArea[T]) = println(m.area(shape))
+  def printPerimeter[T](shape: T)(implicit m: HavePerimeter[T]) = println(m.perimeter(shape))
 }
