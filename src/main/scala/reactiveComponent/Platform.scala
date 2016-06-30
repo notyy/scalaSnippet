21beeb3e5f7cd864bac11c3a4adb43ddf08dea5a
notyy <notyycn@gmail.com>
2016-06-30 13:10:46 +0800

    platform can run simple flow

--- src/main/scala/reactiveComponent/Platform.scala
+++ src/main/scala/reactiveComponent/Platform.scala
@@ -1,5 +1,13 @@
 package reactiveComponent
 
-object Platform {
+import reactiveComponent.framework.{Flow, SimpleTransformation}
 
+object Platform {
+  def run[A, B](simpleTransformation: SimpleTransformation[A, B], A: A): B = {
+    simpleTransformation.update(A)
+  }
+
+  def run[A, B](flow: Flow[A, B], input: A): B = {
+    flow.runThrough(input)
+  }
 }
