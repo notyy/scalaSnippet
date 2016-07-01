26838de47897554e50f0e33ee2eaa4b58499807f
notyy <notyycn@gmail.com>
2016-07-01 15:20:38 +0800

    refactor Stateful component

--- src/main/scala/reactiveComponent/framework/ReactiveComponent.scala
+++ src/main/scala/reactiveComponent/framework/ReactiveComponent.scala
@@ -10,27 +10,36 @@ trait SimpleTransformation[A, B] extends ReactiveComponent[A, B] {
   def update(input: A): B
 }
 
+trait Result[Input, Output]
+
+case class FutureAction[A,B](futureAction: Future[A]) extends Result[A, B]
+
+case class Output[A,B](output: B) extends Result[A, B]
+
 trait StatefulComponent[Model, A, B] extends ReactiveComponent[A, B] {
 
-  def update(model: Model, input: A): (Model, Option[Future[A]], Option[B])
+  def update(model: Model, input: A): (Model, Result[A, B])
 
   def run(initModel: Model, input: A): Future[B] = {
     runIt(initModel, input)
   }
 
   def runIt(model: Model, input: A): Future[B] = {
-    val (newModel, optFutureInput, optOutput) = update(model, input)
-    (optFutureInput, optOutput) match {
-      case (None, None) => throw new IllegalStateException("future input and output can't both be empty")
-      case (None, Some(output)) => Future {
+    val (newModel, result) = update(model, input)
+    result match {
+      case Output(output) => Future {
         output
       }
-      case (Some(futureInput), Some(_)) => throw new IllegalArgumentException("there are future input to be processed, output will not ignored!!!!")
-      case (Some(futureInput), _) =>
-        futureInput.flatMap {
+      case (FutureAction(futureAction)) =>
+        futureAction.flatMap {
           newInput => runIt(newModel, newInput)
         }
     }
   }
+}
+
+
+object StatefulComponent {
+
 }
 
