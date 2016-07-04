afc43b4cf911cef40eaaa51c3034ab33bf4482f4
notyy <notyycn@gmail.com>
2016-07-04 09:49:03 +0800

    component should use least input they needed

--- src/main/scala/reactiveComponent/nbiot/flow/RRCRejectL2Notify.scala
+++ src/main/scala/reactiveComponent/nbiot/flow/RRCRejectL2Notify.scala
@@ -2,14 +2,13 @@ package reactiveComponent.nbiot.flow
 
 import reactiveComponent.framework.{FutureAction, Output, Result, StatefulComponent}
 import reactiveComponent.nbiot.flow.RRCRejectL2Notify._
-import reactiveComponent.nbiot.source.UL_CCCH_MSG
 
 import scala.concurrent.Future
 
-class RRCRejectL2Notify(val rejectNotify: RRCConnReject => Future[RRCRejectL2Notify.L2Timeout]) extends StatefulComponent[EmptyModel, Input, Boolean] {
-  override def update(model: EmptyModel, input: Input): (EmptyModel, Result[Input, Boolean]) = input match {
-    case L2Input(uL_CCCH_MSG) => {
-      (model, FutureAction(rejectNotify(RRCConnReject(uL_CCCH_MSG.ueId))))
+class RRCRejectL2Notify(val rejectNotify: RRCConnReject => Future[RRCRejectL2Notify.L2Timeout]) extends StatefulComponent[Model, Input, Boolean] {
+  override def update(model: Model, input: Input): (Model, Result[Input, Boolean]) = input match {
+    case Reject(ueId) => {
+      (model, FutureAction(rejectNotify(RRCConnReject(ueId))))
     }
     case (RRCRejectL2Notify.L2Timeout()) => {
       (model, Output(true))
@@ -21,12 +20,12 @@ object RRCRejectL2Notify {
 
   trait Input
 
-  case class L2Input(uL_CCCH_MSG: UL_CCCH_MSG) extends Input
+  case class Reject(ueId: UeId) extends Input
 
   case class L2Timeout() extends Input
 
   case class RRCConnReject(ueId: UeId)
 
-  case class EmptyModel()
+  case class Model(ueId: UeId, cellId: CellId)
 
 }
