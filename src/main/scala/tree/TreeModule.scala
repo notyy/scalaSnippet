be3898fc704feeaf02c6507c4c5ccd471dd8f2a4
notyy <notyycn@gmail.com>
2016-09-26 21:45:08 +0800

    make TreeModule runnable

--- src/main/scala/tree/TreeModule.scala
+++ src/main/scala/tree/TreeModule.scala
@@ -1,9 +1,11 @@
 package tree
 
-object TreeModule {
+object TreeModule extends App {
   sealed trait Tree[+T]
 
   case class Branch[T](left: Tree[T],value: T,right: Tree[T]) extends Tree[T]
   case class Leaf[T](value: T) extends Tree[T]
   case object Empty extends Tree[Nothing]
+
+  println(Branch(Empty,1,Leaf(1)))
 }
