103a452174c6927ac410f9ad709357f4050f30fb
notyy <notyycn@gmail.com>
2016-10-09 15:14:52 +0800

    minor refactoring

--- src/main/scala/concurrency/akka/pingpang/Ping.scala
+++ src/main/scala/concurrency/akka/pingpang/Ping.scala
@@ -4,23 +4,25 @@ import akka.actor.{ActorRef, ActorLogging, Actor}
 
 case class Start(opponent: ActorRef)
 
-class Ping extends Actor with ActorLogging{
-  var count = 0
+class Ping extends Actor with ActorLogging {
+  private var count = 0
 
-   override def receive: Receive = {
-     case Start(opponent) => {
-       log.info("starting...")
-       opponent ! "Ping!"
-     }
-     case "Pang!" => {
-       log.info("Pang! received")
-       count += 1
-       if(count < 10) sender() ! "Ping!"
-       else context become tired
-     }
-   }
+  override def receive: Receive = {
+    case Start(opponent) => {
+      log.info("starting...")
+      opponent ! "Ping!"
+    }
+    case "Pang!" => {
+      log.info("Pang! received")
+      count += 1
+      if (count < 10) sender() ! "Ping!"
+      else context become tired
+    }
+  }
 
   def tired: Receive = {
-    case _ =>
+    case _ => {
+      log.info("already tried")
+    }
   }
- }
+}
