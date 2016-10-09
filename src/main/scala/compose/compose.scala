1d4c28bbf525563e7ebe542bdb3de8e62872f791
notyy <notyycn@gmail.com>
2016-10-09 15:13:07 +0800

    a function composition example

--- src/main/scala/compose/compose.scala
+++ src/main/scala/compose/compose.scala
@@ -1,8 +1,17 @@
 package compose
 
-object compose {
+object compose extends App {
 
-  def split: String => List[String] = s => s.split(",").toList
+  def add1: Int => Int = ??? //_ + 1
 
-//  def toInts: List[String] => List[Int] =
+  def multi2: Int => Int = _ * 2
+
+  def int2String: Int => String = _.toString
+
+  def add1ThenMulti2: Int => Int = x => multi2(add1(x))
+
+  def compose[A, B, C](f1: A => B)(f2: B => C): A => C = x => f2(f1(x))
+
+  import withLog.WithLogModule._
+  withLog("add1thenMulti2")(compose(add1)(multi2))(2)
 }
