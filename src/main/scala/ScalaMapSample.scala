9e5f04dc416f665782adf8f6a43f709b9c1aacf8
notyy <notyycn@gmail.com>
2016-10-09 15:17:52 +0800

    make ScalaMapSample an App to easy demo

--- src/main/scala/ScalaMapSample.scala
+++ src/main/scala/ScalaMapSample.scala
@@ -1,7 +1,5 @@
-object ScalaMapSample {
+object ScalaMapSample extends App {
   def process(xs: List[Int]): List[Int] = {
-    xs.par.filter(_ > 2).map(_ * 2)
-
     xs.par.filter(x => {
       println(s"filtering $x in thread ${Thread.currentThread().getName}")
       x > 2
@@ -10,4 +8,10 @@ object ScalaMapSample {
       x * 2
     }).toList
   }
+
+  process(List(1,2,3,4,5))
 }
+
+
+
+//    xs.par.filter(_ > 2).map(_ * 2).toList
