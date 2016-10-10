43402ee153197dd831a7867cf0868b9bb0cf6fc2
notyy <notyycn@gmail.com>
2016-10-10 12:32:45 +0800

    add isEmpty&getOrElse

--- src/main/scala/adt/OptionalModule.scala
+++ src/main/scala/adt/OptionalModule.scala
@@ -2,10 +2,26 @@ package adt
 
 object OptionalModule {
 
-  sealed trait Optional[+T]
+  sealed trait Optional[+T]{
+    def get: T
+    def isEmpty: Boolean
+    def getOrElse[B >: T](default: B):B
+  }
 
-  case class Just[T](value: T) extends Optional[T]
+  case class Just[T](value: T) extends Optional[T] {
+    override def get: T = value
 
-  case object NotExist extends Optional[Nothing]
+    override def isEmpty: Boolean = false
+
+    override def getOrElse[B >: T](default: B): B = value
+  }
+
+  case object NotExist extends Optional[Nothing] {
+    override def get: Nothing = ???
+
+    override def isEmpty: Boolean = true
+
+    override def getOrElse[B >: Nothing](default: B): B = default
+  }
 
 }
