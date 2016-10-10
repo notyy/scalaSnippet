43aa333b2fbe63fc1274c93c792f59d82f6e278b
notyy <notyycn@gmail.com>
2016-10-10 12:33:10 +0800

    minor modification and reformat

--- src/main/scala/adt/TreeModule.scala
+++ src/main/scala/adt/TreeModule.scala
@@ -5,33 +5,36 @@ import adt.ListModule.{Cons, MyList, MyNil}
 import adt.OptionalModule.{Just, NotExist, Optional}
 
 object TreeModule extends App {
-  sealed trait Tree[+T]{
+
+  sealed trait Tree[+T] {
     def map[R](f: T => R): Tree[R]
   }
 
-  case class Branch[T](left: Tree[T],value: T,right: Tree[T]) extends Tree[T] {
-    override def map[R](f: (T) => R): Tree[R] = Branch(left.map(f),f(value),right.map(f))
+  case class Branch[T](left: Tree[T], value: T, right: Tree[T]) extends Tree[T] {
+    override def map[R](f: (T) => R): Tree[R] = Branch(left.map(f), f(value), right.map(f))
   }
+
   case class Leaf[T](value: T) extends Tree[T] {
     override def map[R](f: (T) => R): Tree[R] = Leaf(f(value))
   }
+
   case object Empty extends Tree[Nothing] {
     override def map[R](f: (Nothing) => R): Tree[R] = Empty
   }
 
-  private val bigTree:Tree[Int] = Branch(Leaf(2), 1, Branch(Leaf(3),4,Leaf(5)))
+  private val bigTree: Tree[Int] = Branch(Leaf(2), 1, Branch(Leaf(3), 4, Empty))
 
   def rightMost[T]: Tree[T] => Optional[T] = {
     case Leaf(v) => Just(v)
-    case Branch(_,_,right) => rightMost(right)
+    case Branch(_, _, right) => rightMost(right)
     case Empty => NotExist
   }
 
-  println(s"right most value is ${rightMost(bigTree)}")
+  println(s"right most value is ${rightMost(bigTree).getOrElse(-1) * 2}")
 
   def allMulti2(tree: Tree[Int]): Tree[Int] = map(tree)(_ * 2)
 
-//  def map[A,B](tree: Tree[A])(f: A => B): Tree[B] = tree.map(f)
+  //  def map[A,B](tree: Tree[A])(f: A => B): Tree[B] = tree.map(f)
 
   println(allMulti2(bigTree))
 
