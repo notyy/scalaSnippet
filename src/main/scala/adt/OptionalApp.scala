165ef0575035b35487e9db1e4efd35a6b0b81807
notyy <notyycn@gmail.com>
2016-10-17 17:19:55 +0800

    naive implemation of applicative

--- src/main/scala/adt/OptionalApp.scala
+++ src/main/scala/adt/OptionalApp.scala
@@ -6,10 +6,24 @@ object OptionalApp extends App {
 
   case class User(name: String, age: Int, email: String)
 
+  def getUserByNameSimple: String => User = ???
+
   def getUserByName: String => Optional[User] = {
     case "damotou" => Just(User("damotou", 39, "notyycn@gmail.com"))
     case _ => NotExist
   }
 
   def getAge: User => Int = _.age
+
+  def fetchGetAgeFunc: Optional[User => Int] = Just((user:User) => user.age)
+
+  getUserByName("damotou").map(getAge)
+
+  getUserByName("damotou") match {
+    case Just(user) => fetchGetAgeFunc match {
+      case Just(func) => Just(func(user))
+      case NotExist => NotExist
+    }
+    case NotExist => NotExist
+  }
 }
