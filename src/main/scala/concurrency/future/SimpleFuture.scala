3dc70856ccc4e8450cd6856c098ddbe504df1166
notyy <notyycn@gmail.com>
2016-11-15 14:57:05 +0800

    add Try sample

--- src/main/scala/concurrency/future/SimpleFuture.scala
+++ src/main/scala/concurrency/future/SimpleFuture.scala
@@ -1,11 +1,18 @@
 package concurrency.future
 
-object SimpleFuture {
+import com.typesafe.scalalogging.slf4j.StrictLogging
+
+import scala.util.Try
+
+object SimpleFuture extends App with StrictLogging {
   def add(x: Int, y: Int): Int = x + y
 
   def concat(x: Int, y: Int): List[Int] = List(x, y)
 
   def divide(x: Int, y: Int): Option[Int] = if (y == 0) None else Some(x / y)
 
-  def sum(xs: List[Int]): Future[Int] = ???
+  def safeDiv(x: Int, y: Int): Try[Int] = Try {
+    x / y
+  }
+
 }
