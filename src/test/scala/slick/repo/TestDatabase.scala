a79336447a13ecb38386c8b6b47d1e4d0e0a24d1
notyy <notyycn@gmail.com>
2017-09-14 17:30:51 +0800

    add TestDatabase trait

---
+++ src/test/scala/slick/repo/TestDatabase.scala
@@ -0,0 +1,14 @@
+package slick.repo
+import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.StrictLogging
+import slick.basic.DatabaseConfig
+import slick.{DBConfigProvider, Database}
+
+trait TestDatabase {
+  val myDatabase = new Database with DBConfigProvider with StrictLogging {
+    override def getDatabaseConfig(dbName: String) = {
+      val testConfig = ConfigFactory.load("test")
+      DatabaseConfig.forConfig(dbName, testConfig)
+    }
+  }
+}
