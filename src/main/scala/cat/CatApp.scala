486dbb9edfbf0307b3892d82ce26fb5f378b083e
notyy <notyycn@gmail.com>
2017-10-13 15:34:00 +0800

    add a pair monoid example

--- src/main/scala/cat/CatApp.scala
+++ src/main/scala/cat/CatApp.scala
@@ -9,17 +9,28 @@ object CatApp extends App {
     def combine(x: Int, y: Int): Int = x + y
   }
 
-  implicit val intMultiMonoid: Monoid[Int] = new Monoid[Int] {
-    override def empty:Int = 1
-    override def combine(x: Int, y: Int) = x * y
-  }
+  //have to comment this out, or the combine pair example will fail
+//  implicit val intMultiMonoid: Monoid[Int] = new Monoid[Int] {
+//    override def empty:Int = 1
+//    override def combine(x: Int, y: Int) = x * y
+//  }
 
   implicit val strAdditionMonoid: Monoid[String] = new Monoid[String] {
     def empty: String = ""
     def combine(x: String, y: String): String = x ++ y
   }
 
+  implicit def pairMonoid[A,B](implicit a: Monoid[A], b: Monoid[B]): Monoid[(A,B)] =
+    new Monoid[(A,B)] {
+      override def empty: (A, B) = (a.empty, b.empty)
+
+      override def combine(x: (A, B), y: (A, B)): (A, B) = {
+        (a.combine(x._1, y._1), b.combine(x._2,y._2))
+      }
+    }
+
   println(combineAll(List(1,2,3,4,5))(intAdditionMonoid))
-  println(combineAll(List(1,2,3,4,5))(intMultiMonoid))
+//  println(combineAll(List(1,2,3,4,5))(intMultiMonoid))
   println(combineAll(List("1","2","3")))
+  println(combineAll(List((1,2),(3,4),(5,6))))
 }
