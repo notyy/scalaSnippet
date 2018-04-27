79dfe992d41039eb4b2616c970754acafcaca00d
notyy <notyycn@gmail.com>
2018-04-27 11:04:49 +0800

    refine Future example

---
+++ src/main/scala/concurrency/futurePractice/FuturePractice.scala
@@ -0,0 +1,67 @@
+package concurrency.futurePractice
+
+import com.typesafe.scalalogging.StrictLogging
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.io.StdIn
+import scala.util.{Failure, Success, Try}
+
+object FuturePractice extends App with StrictLogging {
+  def greeting: String = {
+    logger.info("creating greeting")
+    "hello,world"
+  }
+
+  def greetingFuture: Future[String] = Future {
+//    throw new IllegalStateException("BAD BAD")
+    logger.info("creating greeting")
+    "hello,world from greeting future"
+  }
+
+  def anotherFuture: Future[String] = Future {
+//        throw new IllegalStateException("BAD BAD")
+    logger.info("creating another future")
+    "hello,world from another future"
+  }
+
+  //fallbackTo is not lazy, both future will be created and run
+  val future =  greetingFuture.fallbackTo(anotherFuture)
+
+  future.onComplete{
+    case Success(greeting) => println(greeting)
+    case Failure(e) => logger.error("error running future", e)
+  }
+
+////  greetingFuture.fallbackTo()
+//
+//  logger.info("let's begin")
+//  //  logger.info(s"i say $greetingFuture")
+//  val lastFuture = greetingFuture.flatMap { s =>
+//    Future {
+//      throw new IllegalArgumentException("xx wrong")
+//      logger.info("creating xx")
+//      s + "-xx"
+//    }
+//  }.map { s =>
+//      logger.info("creating yy")
+//      s + "-yy"
+//  }
+////  logger.info(s"i say: ${Await.result(lastFuture, 2 seconds)}")
+//  //  greetingFuture.foreach(s => logger.info(s"i say: $s"))
+//  lastFuture.onComplete{
+//    case Success(x) => logger.info(x)
+//    case Failure(e) => logger.error(e.getMessage,e)
+//  }
+//  logger.info("something")
+//
+//  def processResult: Try[String] => String = {
+//    case Success(s) => logger.info(s"i say: $s"); "x"
+//    case Failure(e) => logger.error(s"error occures: ${e.getMessage}", e); "y"
+//  }
+
+  println("press enter to close")
+  StdIn.readLine()
+}
+
