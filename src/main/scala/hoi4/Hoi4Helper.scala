c77f9d65719aa62aac07ebe777d2dc030929afc0
notyy <notyycn@gmail.com>
2018-12-07 17:03:58 +0800

    upgrade hoi4 alg

--- src/main/scala/hoi4/Hoi4Helper.scala
+++ src/main/scala/hoi4/Hoi4Helper.scala
@@ -8,82 +8,27 @@ object Hoi4Helper {
   val artillery2 = Equipment("artillery2", 4.0)
   val antiTank1 = Equipment("anti tank gun1", 4.0)
   val antiTank2 = Equipment("anti tank gun2", 5.0)
-  val mech = Equipment("mech infantry", 12.0)
+  val mech = Equipment("mech infantry1", 8.0)
   val support = Equipment("support", 4.0)
   val lightArmor2 = Equipment("light armor2", 9.0)
   val lightArmor3 = Equipment("light armor3", 10.0)
+  val mediumTank1 = Equipment("medium tank1", 12.0)
   val mediumTank3 = Equipment("medium tank3", 13.0)
+  val mediumSelfArtillery1 = Equipment("medium self artillery1", 12.0)
   val mediumSelfArtillery3 = Equipment("medium self artillery3", 13.0)
   val lightSelfArtillery2 = Equipment("lightSelfArtillery2", 9.0)
   val lightSelfArtillery3 = Equipment("lightSelfArtillery3", 10.0)
   val selfRocket = Equipment("self rocket", 12.0)
   val motorized = Equipment("motorized", 2.50)
-  val f3f = Equipment("f3f", 28.40)
+  val carrierFighter1 = Equipment("carrierFighter1", 28.40)
+  val carrierFighter2 = Equipment("carrierFighter2", 30.40)
 
-  val equipmentTypes = List(smallArms2, artillery2, antiTank1, support, lightArmor2,
-    lightSelfArtillery2, motorized)
-
-  val basicProduct39: Double = 5 * 1.60 * 0.8008
+  val basicProduct: Double = 5 * (1+ 0.15 + 0.189 + 0.60) * 0.9009
 
   case class DivisionType(name: String, equipments: Map[Equipment, Int])
 
-  val infantry41 = DivisionType("infantry 41", Map(
-    smallArms2 -> 710,
-    artillery2 -> 96,
-    antiTank1 -> 24,
-    support -> 30
-  ))
-  val marine40 = DivisionType("marine 40", Map(
-    smallArms2 -> 1100,
-    artillery2 -> 96,
-    support -> 40
-  ))
-  val lightArmor41 = DivisionType("light armor 41", Map(
-    lightArmor2 -> 300,
-    smallArms2 -> 250,
-    antiTank1 -> 24,
-    artillery2 -> 24,
-    motorized -> 100,
-    support -> 40,
-    lightSelfArtillery2 -> 72
-  ))
-  val motorized41 = DivisionType("motorized 41", Map(
-    smallArms2 -> 710,
-    antiTank1 -> 24,
-    artillery2 -> 24,
-    motorized -> 380,
-    support -> 30,
-    selfRocket -> 40
-  ))
-  val motorized42 = DivisionType("motorized 42", Map(
-    smallArms2 -> 710,
-    antiTank2 -> 24,
-    artillery2 -> 24,
-    motorized -> 380,
-    support -> 30,
-    selfRocket -> 40
-  ))
-  val tank42 = DivisionType("tank 42", Map(
-    antiTank2 -> 24,
-    artillery2 -> 24,
-    smallArms2 -> 450,
-    mech -> 200,
-    support -> 40,
-    mediumTank3 -> 150,
-    mediumSelfArtillery3 -> 72
-  ))
-  val mech42 = DivisionType("mech 42", Map(
-    antiTank2 -> 24,
-    artillery2 -> 24,
-    selfRocket -> 40,
-    smallArms2 -> 710,
-    mech -> 350,
-    motorized -> 30,
-    support -> 30
-  ))
-
   def requiredFactoryToProduceInOneYear(equipment: Equipment, quantity: Int): Double = {
-    quantity / (basicProduct39 / equipment.cost * 365)
+    quantity / (basicProduct / equipment.cost * 365)
   }
 
   def planFactories(divisions: Map[DivisionType, Int]): Map[String, Double] = {
@@ -110,107 +55,54 @@ object Hoi4Helper {
     divisionType.equipments.mapValues(_ * howManyDivisions)
   }
 
-  val f3fCost = 28.40
-  val tbdCost = 31.20
-  val p40Cost = 21.6
-  val a17Cost = 22.0
-  val xtbd1Cost = 26
-  val f4fCost = 30.4
-  val p51Cost = 25.20
-  val sbdCost = 21.60
-  println(s"to produce 100 f3f, we need ${100 / (basicProduct39 / f3fCost * 365)}")
-  println(s"to produce 100 tbd, we need ${100 / (basicProduct39 / tbdCost * 365)}")
-  println(s"to produce 100 p40, we need ${100 / (basicProduct39 / p40Cost * 365)}")
-  println(s"to produce 100 a17, we need ${100 / (basicProduct39 / a17Cost * 365)}")
-  println(s"to produce 100 xtbd1, we need ${100 / (basicProduct39 / xtbd1Cost * 365)}")
-  println(s"to produce 100 f4f, we need ${100 / (basicProduct39 / f4fCost * 365)}")
-  println(s"to produce 100 p51, we need ${100 / (basicProduct39 / p51Cost * 365)}")
-  println(s"to produce 100 sbd, we need ${100 / (basicProduct39 / sbdCost * 365)}")
-
-  val infrastructureCost: Int = 3000
-  val airbaseCost = 1250
-  val antiAirCost = 2500
-  val radarCost = 3375
-
-  val militaryFactoryCost = 7200
-  val civilianFactoryCost = 10800
-  val shipyardCost = 6400
-
-  val navalBaseCost = 3000
-  val fortressCost = 500
-  val coastFortressCost = 500
-
-  val constructionAbility4NavalBase = 5 * 1.45
-  val constructionAbility4Infrastructure = 5 * 1.65
-
-  val constructionAbility4MilitaryFactory = 5 * 1.80 * 1.5
-  val constructionAbility4CivilianFactory = 5 * 1.45 * 1.5
-  val constructionAbility4Shipyard = 5 * 1.55 * 1.5
-
-  val constructionNum = 180
-  println(s"with $constructionNum constructors, we can construct one of following per year:")
-  println(s"navalBase: ${constructionAbility4NavalBase * constructionNum * 365 / navalBaseCost}")
-  println(s"infrastructures: ${constructionAbility4Infrastructure * constructionNum * 365 / infrastructureCost}")
-  println(s"airbase: ${constructionAbility4Infrastructure * constructionNum * 365 / airbaseCost}")
-  println(s"antiAir: ${constructionAbility4Infrastructure * constructionNum * 365 / antiAirCost}")
-  println(s"radar: ${constructionAbility4Infrastructure * constructionNum * 365 / radarCost}")
-  println(s"fortress: ${constructionAbility4Infrastructure * constructionNum * 365 / fortressCost}")
-  println(s"coastFortress: ${constructionAbility4Infrastructure * constructionNum * 365 / coastFortressCost}")
-
-  println(s"militaryFactory: ${constructionAbility4MilitaryFactory * constructionNum * 365 / militaryFactoryCost}")
-  println(s"civilianFactoryCost: ${constructionAbility4CivilianFactory * constructionNum * 365 / civilianFactoryCost}")
-  println(s"shipyard: ${constructionAbility4Shipyard * constructionNum * 365 / shipyardCost}")
 }
 
 object Hoi4HelperApp extends App {
   import Hoi4Helper._
 
-//  val needToProduce = Map(
-//    infantry41 -> (16 + 16 + 12),
-//    marine40 -> 8,
-//    lightArmor41 -> 8,
-//    motorized42 -> 32,
-//    mech42 -> 8,
-//    tank42 -> 8
-//  )
+  //define Division types
+  val infantry37 = DivisionType("infantry37", Map(
+    smallArms2 -> 750,
+    artillery2 -> 84,
+    support -> 40
+  ))
+  val marine40 = DivisionType("marine", Map(
+    smallArms2 -> 1100,
+    artillery2 -> 84,
+    support -> 70,
+    motorized -> 20
+  ))
+  val motorized41 = DivisionType("motorized41", Map(
+    smallArms2 -> 710,
+    artillery2 -> 12,
+    motorized -> 410,
+    support -> 80,
+    selfRocket -> 40
+  ))
+  val tank40 = DivisionType("tank40", Map(
+    artillery2 -> 12,
+    smallArms2 -> 410,
+    motorized -> 210,
+    support -> 50,
+    mediumTank1 -> 150,
+    mediumSelfArtillery1 -> 72
+  ))
 
-  val northEastArmy = Map(
-    infantry41 -> 12,
-    marine40 -> 4,
-    lightArmor41 -> 4,
-    tank42 -> 4
+  //define armies
+  val asiaDefendArmy = Map(
+    infantry37 -> 12,
+    marine40 -> 3,
+    tank40 -> 4,
+    motorized41 -> 5
   )
-  val chinaArmy = Map(
-    infantry41 -> 12,
-    marine40 -> 4,
-    lightArmor41 -> 4,
-    tank42 -> 4
-  )
-  val southAsiaArmy = Map(
-    infantry41 -> 12,
-    marine40 -> 4,
-    lightArmor41 -> 4,
-    tank42 -> 4
-  )
-  val westEuroArmy1 = Map(
-    motorized42 -> 12,
-    mech42 -> 6,
-    tank42 -> 6
-  )
-  val westEuroArmy2 = Map(
-    motorized42 -> 12,
-    mech42 -> 6,
-    tank42 -> 6
-  )
-  val westEuroArmy3 = Map(
-    motorized42 -> 12,
-    mech42 -> 6,
-    tank42 -> 6
+
+  val japanArmy = Map(
+    tank40 -> 8,
+    motorized41 -> 16
   )
 
   import concurrency.akkaStream.MapUtil._
-  val needToProduce = mergeMaps(northEastArmy,chinaArmy,southAsiaArmy,westEuroArmy1,westEuroArmy2,westEuroArmy3)(_ + _)
-
+  val needToProduce = mergeMaps(asiaDefendArmy,japanArmy)(_ + _)
 
   val title: String = needToProduce.toSet[(DivisionType,Int)].map {
     case (divisionType:DivisionType,quantity:Int) => s"$quantity ${divisionType.name}"
--- src/test/scala/hoi4/Hoi4HelperTest.scala
+++ src/test/scala/hoi4/Hoi4HelperTest.scala
@@ -4,28 +4,42 @@ import org.scalatest.{FunSpec, Matchers}
 import Hoi4Helper._
 
 class Hoi4HelperTest extends FunSpec with Matchers {
+
+  val infantry37 = DivisionType("infantry37", Map(
+    smallArms2 -> 750,
+    artillery2 -> 84,
+    support -> 40
+  ))
+
+  val motorized41 = DivisionType("motorized41", Map(
+    smallArms2 -> 710,
+    artillery2 -> 12,
+    motorized -> 410,
+    support -> 80,
+    selfRocket -> 40
+  ))
+
   describe("Hoi4Helper") {
     it("can calc how many equipment it needs for a given number of divisions") {
-      val equipments = divisionEquiptments(infantry41, 2)
-      equipments should have size 4
-      equipments(smallArms2) shouldBe 1420
-      equipments(artillery2) shouldBe 192
-      equipments(antiTank1) shouldBe 48
-      equipments(support) shouldBe 60
+      val equipments = divisionEquiptments(infantry37, 2)
+      equipments should have size 3
+      equipments(smallArms2) shouldBe 1500
+      equipments(artillery2) shouldBe 168
+      equipments(support) shouldBe 80
     }
     it("can calc total equipments requirement by equipment types") {
-      val division1Equipments: Map[Equipment, Int] = infantry41.equipments
-      val division2Equipments: Map[Equipment, Int] = lightArmor41.equipments
+      val division1Equipments: Map[Equipment, Int] = infantry37.equipments
+      val division2Equipments: Map[Equipment, Int] = motorized41.equipments
       val total = totalEquipments(List(division1Equipments,division2Equipments))
-      total should have size 7
-      total(smallArms2) shouldBe 960
-      total(artillery2) shouldBe 120
-      total(lightArmor2) shouldBe 300
-      total(motorized) shouldBe 100
-      total(lightSelfArtillery2) shouldBe 72
+      total should have size 5
+      total(smallArms2) shouldBe 1460
+      total(artillery2) shouldBe 96
+      total(motorized) shouldBe 410
+      total(selfRocket) shouldBe 40
+      total(support) shouldBe 120
     }
     it("can calc required factory number for a given equipment to be produce in one year"){
-      requiredFactoryToProduceInOneYear(f3f, 100) shouldBe 1.2145388857717625
+      requiredFactoryToProduceInOneYear(carrierFighter1, 100) shouldBe 0.8908428020101175
     }
   }
 }
